37a3d12af1f88fbf3c626430e4d7e9b19c285598
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2025-02-16 11:56:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,23 @@
 from os import getenv
 
 # --------------------------------------------------------
+
+API_ID = int(os.getenv("API_ID","14050586"))
+# --------------------------------------------------------
+API_HASH = os.getenv("API_HASH","42a60d9c657b106370c79bb0a8ac560c")
+# --------------------------------------------------------
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+# --------------------------------------------------------
+BOT_USERNAME = os.getenv("BOT_USERNAME","rishu_musicbot")
+# --------------------------------------------------------
+MONGO_URI = os.getenv("MONGO_URI","mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority")
+# --------------------------------------------------------
+OWNER_ID = int(os.getenv("OWNER_ID","5738579437"))
+# --------------------------------------------------------
+FORCE_JOIN1 = os.getenv("FORCE_JOIN1","RishuApi")
+# --------------------------------------------------------
+FORCE_JOIN2 = os.getenv("FORCE_JOIN2","rishucoder")
+# --------------------------------------------------------
+
 BOT_USERNAME = getenv("BOT_USERNAME", "Your_Anshika_bot")
+# --------------------------------------------------------
